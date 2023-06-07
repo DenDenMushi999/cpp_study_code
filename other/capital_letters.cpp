b79ba264dc20d3694e45fd32957116da3be34aef
denis_ubuntu_laptop <den.davletshin999@gmail.com>
2023-06-07 17:40:36 +0300

    capital letters

---
+++ other/capital_letters.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void Print(){
+    std::cout << "hello" << std::endl;
+}
+
+int main(){
+    int Capital = 1;
+    int capital = 2;
+    std::cout << Capital << " " << capital << std::endl;
+    // print();
+    Print();
+}
